70db1c5cf1427a5af44f610d8d03f3c6675aa2ae
ljb-k <354275172@qq.com>
2017-04-14 11:07:21 +0800

    Initial commit

---
+++ app/src/main/java/com/example/dabaoandfanbianyi/MainActivity.java
@@ -0,0 +1,38 @@
+package com.example.dabaoandfanbianyi;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    private Button btn;
+    private Button btnChange;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        btn = (Button) findViewById(R.id.btn);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this,"按钮被点击",Toast.LENGTH_LONG).show();
+            }
+        });
+
+        btnChange = (Button) findViewById(R.id.btnChange);
+        btnChange.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setClass(MainActivity.this,ActivityB.class);
+                startActivity(intent);
+
+            }
+        });
+    }
+}
